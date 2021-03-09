815d7df66398f5347e6c86e40c087ba7748ff948
admin <admin@qq.com>
2021-03-09 21:40:06 +0800

    新建登录分支

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const Login = () => import('../views/login/Login')
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    }
   ]
 })
